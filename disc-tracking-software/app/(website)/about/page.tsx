d8341cef4160bdf53ba43ee57aa6148ab31c913e
Jaden Hampton <techiesbestfriend@gmail.com>
2026-02-27 10:37:05 -0700

    Website subpages

---
+++ disc-tracking-software/app/(website)/about/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function About() {
+  return (
+    <div className="pt-20 md:pt-24 min-h-screen bg-[#190f2A] text-white">
+        <div className="text-white text-3xl font-bold p-8 m-8 flex items-center justify-center">
+            About Us
+        </div>
+        <div className="text-white text-lg p-0 m-8 flex items-center justify-center">
+            We are a team of passionate developers and disc golf enthusiasts who wanted to create a software that can help players track their progress and improve their game. Our software uses advanced algorithms to analyze your throws and provide you with insights on how to improve your technique. We are committed to providing our users with the best experience possible and are constantly working on new features and improvements.
+        </div>
+    </div>
+  )
+}
---
+++ disc-tracking-software/app/(website)/terms-conditions/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function TermsConditions() {
+  return (
+    <div>page</div>
+  )
+}
